0435c4ee8846aeae6ea264bd38b15dc4f45d1241
Sam Haekens <sam@groupadw.be>
2019-03-28 15:26:03 +0100

    Connection Class

---
+++ Syntra.Eindproject.Dapper/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntra.Eindproject.Dapper
+{
+    public class Connection
+    {
+        private static readonly Connection _connection = new Connection();
+
+        public static Connection Instance => _connection;
+
+        private string _connectionString;
+
+        public string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_connectionString)
+                {
+                    throw new Exception("Set connectionString first");
+                }
+                return _connectionString;
+            }
+            private set => _connectionString = value;
+        }
+
+        public void SetConnection(string connectionString)
+        {
+            if (!string.IsNullOrWhiteSpace(_connectionString))
+            {
+                throw new Exception("Connection can only be set once");
+            }
+            ConnectionString = connectionString;
+        }
+
+        private Connection() { }
+    }
+}
---
+++ Syntra.Eindproject.WPF/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using Syntra.Eindproject.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntra.Eindproject.WPF
+{
+    public class DatabaseManager
+    {
+        private static readonly DatabaseManager _manager = new DatabaseManager();
+
+        public static DatabaseManager Instance => _manager;
+
+        private DatabaseManager()
+        {
+            Connection.Instance.SetConnection("server=Eindproject; database=Syntra; User id= Syntra; password=Syntra123!");
+        }
+    }
+}
