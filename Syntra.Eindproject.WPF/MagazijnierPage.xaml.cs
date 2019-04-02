f934249491ed90c155f61971986671d8370aa22d
sam <sam_haekens@hotmail.com>
2019-04-02 18:51:11 +0200

    Part2 2/04/2019

--- Syntra.Eindproject.WPF/MagazijnierPage.xaml.cs
+++ Syntra.Eindproject.WPF/MagazijnierPage.xaml.cs
@@ -31,10 +31,14 @@ namespace Syntra.Eindproject.WPF
             TxtNaam.Text = string.Empty;
             TxtSoort.Text = string.Empty;
             TxtOorsprong.Text = string.Empty;
+            TxtId.Text = string.Empty;
+            TxtPrijs.Text = string.Empty;
+            TxtEenheid.Text = string.Empty;
+            TxtVervalDatum.Text = string.Empty;
 
             List<Product> products = DatabaseManager.Instance.ProductRepository.GetProducts().ToList();
 
-            LbProducts.ItemsSource = products;
+            LbProducts.ItemsSource = products; //Listview DisplayMemberBinding
 
             //foreach (Product product in products)
             //{
@@ -52,7 +56,7 @@ namespace Syntra.Eindproject.WPF
             double.TryParse(TxtPrijs.Text, out double prijs);
             int.TryParse(TxtId.Text, out int id);
             var dateAndTime = DateTime.Now;
-            var date = dateAndTime.Date;
+            var date = dateAndTime.Date;                              //.ToShortDateString();
 
             Product product = new Product(id, TxtNaam.Text, TxtSoort.Text, TxtOorsprong.Text, prijs, TxtEenheid.Text, date, date);
             DatabaseManager.Instance.ProductRepository.InsertProduct(product);
@@ -66,22 +70,29 @@ namespace Syntra.Eindproject.WPF
 
             if (product != null)
             {
+                TxtId.Text = product.Id.ToString();
                 TxtNaam.Text = product.Naam;
                 TxtSoort.Text = product.Soort;
                 TxtOorsprong.Text = product.Oorsprong;
+                TxtPrijs.Text = product.Prijs.ToString();
+                TxtEenheid.Text = product.Eenheid;
             }
         }
 
         private Product GetSelectedProduct()
         {
-            if (LbProducts.SelectedItem != null)
-            {
-                ListViewItem item = (ListViewItem)LbProducts.SelectedItem;
-                Product product = (Product)item.Tag;
+            //if (LbProducts.SelectedItem != null)
+            //{
+            //    ListViewItem item = (ListViewItem)LbProducts.SelectedItem;
+            //    Product product = (Product)item.Tag;
 
-                return product;
-            }
-            return null;
+            //    return product;
+            //}
+            //return null;
+
+            Product current = LbProducts.SelectedItem as Product;
+
+            return current;
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
